08193dc60c8afd384777b27ff4e28ef3f75a9feb
DashaZhirenok <dashazhirenok@yandex.ru>
2019-05-14 20:46:34 +0400

    library/API_commands + base test cases

---
+++ tests/create_character.py
@@ -0,0 +1,10 @@
+from library.API_commands import APICommands
+
+
+class TestSuiteCreateCharacter:
+
+    def setup(self):
+        print("basic setup into class")
+
+    def teardown(self):
+        print("basic teardown into class")
---
+++ tests/get_all_characters.py
@@ -0,0 +1,21 @@
+from library.API_commands import APICommands
+
+
+class TestSuiteGetAllCharacters:
+
+    def setup(self):
+        print("basic setup into class")
+
+    def teardown(self):
+        print("basic teardown into class")
+
+    def test_get_all_characters_by_valid_user(self):
+        commands = APICommands()
+        all_characters, status_code = commands.get_all_characters()
+        assert str(status_code) == "200"
+
+    def test_get_all_characters_by_invalid_user(self):
+        commands = APICommands()
+        all_characters, status_code = commands.get_all_characters("some_login", "some_password")
+        assert str(status_code) == "401"
+
